cb79781e253b7ec699ff8536c2f628ded30590aa
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-31 21:37:06 +0530

    Create a Contact in Address Book

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace AddressbookDemo
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            AddressBook addressBook = new AddressBook();
+
+            addressBook.Name = ("Prateek");
+            addressBook.Address = ("Bangalore");
+            addressBook.MobileNo = (9945007207);
+            addressBook.City = ("Bangalore");
+            addressBook.Email = ("Prateekvasanthpai@gmail.com");
+            addressBook.ZipCode = (5760027);
+            addressBook.State = ("Karnataka");
+
+            Console.WriteLine("Name:{0}",addressBook.Name);
+            Console.WriteLine("Address: {0}",addressBook.Address);
+            Console.WriteLine("City: {0}", addressBook.City);
+            Console.WriteLine("State: {0}", addressBook.State);
+            Console.WriteLine("Zip Code: {0}", addressBook.ZipCode);
+            Console.WriteLine("Mobile: {0}", addressBook.MobileNo);
+            Console.WriteLine("Email: {0}", addressBook.Email);
+
+
+        }
+    }
+}
