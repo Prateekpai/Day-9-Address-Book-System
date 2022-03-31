3ad755b57c0e46b1baa9d34ba486692d99884419
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-31 22:00:39 +0530

    Edit Contact in Address Book

---
+++ AddressBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressbookDemo
+{
+    public class AddressBook
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long MobileNo { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+
+    }
+
+}
